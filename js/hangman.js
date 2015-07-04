dc1371e15114383abbd1c11dee22bc4b88c88b5f
Paul Boon <gvr37.p.l.boon@gmail.com>
2015-07-05 00:16:07 +0200

    made some functions made button bigger added counter and guessedchars

--- js/hangman.js
+++ js/hangman.js
@@ -5,33 +5,79 @@ $( document ).ready(function() {
     var allowedChars = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
     var guessedChars = [];
     var nOfWrongGuesses = 0;
-    var answer = "";
-    for(var i = 0;i<word.length;i++){
-        answer += "-"
-    }
-    $("#answer").val(answer);
-    console.log(word);
+    var maxWrongGuesses = 7;
+    var answer;
+    
+    reset();
     
     $("#submit").on("click",function(){
         var guess = $("#guess").val();
+        if(guess == ""){
+            return;
+        }
+        guessedChars.push(guess);
         
         guessedRight = false;
         for(var i = 0;i<word.length;i++){
             if(guess == word[i]){
                 guessedRight = true;
-                guessedChars.push(guess);
                 break;
             }
         }
         if(!guessedRight){
             nOfWrongGuesses++;
-            if(nOfWrongGuesses > 7){
-                word = words[Math.floor(Math.random() * words.length)];
-                console.log(word);
-                nOfWrongGuesses = 0;
+            if(nOfWrongGuesses >= maxWrongGuesses){
                 alert("u got rekt m8");
+                reset();
             }
         }
+        set();
+        if(answer == word){
+            alert("such win");
+            reset();
+        }
+        $("#guess").val("");
+    });
+    
+    $("#guess").on("keyup",function(e){
+        var input = String.fromCharCode(e.keyCode).toLowerCase();
+        for(var i = 0;i< guessedChars.length;i++){
+            if(input == guessedChars[i]){
+                $("#alerts").html("character has already been guessed");
+                $("#guess").val("");
+                return;
+            }
+        }
+        for(var i = 0;i< allowedChars.length;i++){
+            if(input == allowedChars[i]){
+                $("#guess").val(input);
+                $("#alerts").html("");
+                return;
+            }    
+        }
+        $("#alerts").html("character is not allowed");
+        $("#guess").val("");
+    });
+    
+    $("#guess").on("keydown",function(e){
+        $("#guess").val("");
+    });
+    
+    function reset(){
+        word = words[Math.floor(Math.random() * words.length)];
+        console.log(word);
+        guessedChars = [];
+        nOfWrongGuesses = 0;
+        set();
+    }
+    
+    function set(){
+        $("#guessedChars").html("guessed characters: " + guessedChars);
+        $("#counter").html("number of wrong guesses: " + nOfWrongGuesses + "/" + maxWrongGuesses);
+        setAnswer();
+    }
+    
+    function setAnswer(){
         answer = "";
         for(var i = 0;i<word.length;i++){
             var found = false;
@@ -45,44 +91,8 @@ $( document ).ready(function() {
             if(!found){
                 answer += "-";
             }
-            
         }
         $("#answer").val(answer);
-        if(answer == word){
-            alert("such win");
-            word = words[Math.floor(Math.random() * words.length)];
-            console.log(word);
-            nOfWrongGuesses = 0;
-            guessedChars = [];
-            answer = "";
-            for(var i = 0;i<word.length;i++){
-                answer += "-"
-            }
-            $("#answer").val(answer);
-        }
-    });
-    
-    $("#guess").on("keyup",function(e){
-        var input = String.fromCharCode(e.keyCode).toLowerCase();
-        for(var i = 0;i< guessedChars.length;i++){
-            if(input == guessedChars[i]){
-                $("#alerts").replaceWith("character has already been guessed");
-                $("#guess").val("");
-                return;
-            }
-        }
-        for(var i = 0;i< allowedChars.length;i++){
-            if(input == allowedChars[i]){
-                $("#guess").val(input);
-                return;
-            }    
-        }
-        $("#alerts").replaceWith("character is not allowed");
-        $("#guess").val("");
-    });
-    
-    $("#guess").on("keydown",function(e){
-        $("#guess").val("");
-    });
+    }
     
 });
