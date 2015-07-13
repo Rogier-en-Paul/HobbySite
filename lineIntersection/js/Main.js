96b6f5f3cdf7cecf3d4e1990ec0b76fe93f04d47
Paul Boon <gvr37.p.l.boon@gmail.com>
2015-07-13 22:15:43 +0200

    added a triangle added an isInTriangle method to Point added Points for triangle and a getDistande functin changed title

--- lineIntersection/js/Main.js
+++ lineIntersection/js/Main.js
@@ -3,9 +3,15 @@ canvas.addEventListener("mousemove", doMouseMove,false);
 var ctxt = canvas.getContext("2d");
 
 var Point1 = new Point(10,10);
-var Point2 = new Point(60,60);
+var Point2 = new Point(50,50);
 var Line = new Line(Point1,Point2);
 
+var Point3 = new Point(100,400);
+var Point4 = new Point(250,100);
+var Point5 = new Point(400,400);
+var Triangle = new Triangle(Point3,Point4,Point5);
+
+update();
 
 function doMouseMove(e){
     var mousePos = getMousePos(canvas,e);
@@ -17,7 +23,9 @@ function doMouseMove(e){
 function update(){
     ctxt.clearRect(0, 0, canvas.width, canvas.height);
     Point1.drawPoint(ctxt);
+    console.log(Point1.isInTriangle(Triangle));
     Line.drawLine(ctxt);
+    Triangle.drawTriangle(ctxt);
 }
 
 function getMousePos(canvas,e){
@@ -26,4 +34,10 @@ function getMousePos(canvas,e){
         x: e.clientX - rect.left,
         y: e.clientY - rect.top
     };
+}
+
+function getDistance(Point1,Point2){
+    var a = Point1.x - Point2.x;
+    var b = Point1.y - Point2.y;
+    return Math.pow(Math.pow(a,2)+Math.pow(b,2),0.5);
 }
--- lineIntersection/js/Point.js
+++ lineIntersection/js/Point.js
@@ -2,8 +2,16 @@ function Point(x,y){
     this.x = x;
     this.y = y;
 
-    this. drawPoint = function(ctxt){
+    this.drawPoint = function(ctxt){
         var size = 10;
         ctxt.fillRect(this.x - size/2, this.y-size/2, size, size);
+    };
+
+    this.isInTriangle = function(Triangle){
+        var sideA = getDistance(Triangle.points[0],this);
+        var sideB = getDistance(Triangle.points[1],this);
+        var sideC = getDistance(Triangle.points[0],Triangle.points[1]);
+        var Y = Math.acos((Math.pow(sideA,2)-Math.pow(sideB,2)-Math.pow(sideC,2))/2*sideA*sideB);
+        return false;
     }
 }
