272e1640e104f6806c4bc12a0efb76ad068c360d
Paul Boon <gvr37.p.l.boon@gmail.com>
2015-07-14 21:27:10 +0200

    made object delegation actually work

--- javascriptClasses/js/Dog.js
+++ javascriptClasses/js/Dog.js
@@ -5,5 +5,5 @@ function Dog (name,legs) {
 Dog.prototype = Object.create(Animal.prototype);
 //Dog.prototype.constructor = Dog;
 Dog.prototype.howl = function(){
-    console.log("woef"+ this.name);
+    console.log("woef"+ this.name + " :" + this.legs);
 };
--- javascriptClasses/js/Main.js
+++ javascriptClasses/js/Main.js
@@ -1,22 +1,16 @@
-var dog1 = new Dog("Paul",10);
-var dog2 = new Dog("test",404);
-var cat1 = new Cat("acat",25);
-var cat2 = new Cat("acat2",2353);
+var dog1 = new Dog("a",1);
+var dog2 = new Dog("b",2);
 
 dog1.howl();
-dog1.howl = function(){
+console.log(dog1.howl === dog2.howl);
+Dog.prototype.howl = function(){
   console.log("test");
 };
 dog2.howl();
 
-cat1.howl();
-cat1.howl = function(){
-    console.log("test");
-};
-cat2.howl();
-dog1.howl = null;
+console.log(dog1.howl === dog2.howl);
+
+delete Dog.prototype.howl;
 dog1.howl();
 
-var body = $("body");
-body.append(dog1.name + "<br/>");
-body.append(dog1.legs + "<br/>");
+
