3282c82657c814ad339530da50ea32f40e46f1ab
Paul Boon <gvr37.p.l.boon@gmail.com>
2015-07-18 16:00:27 +0200

    trying to add some logic but stuff still doesn't work

--- pathFinding/js/Main.js
+++ pathFinding/js/Main.js
@@ -2,8 +2,11 @@ var canvas = document.getElementById("mycanvas");
 var ctxt = canvas.getContext("2d");
 ctxt.fillRect(10,10,10,10);
 
-var startPoint = new Point(1,2);
 var destinationPoint = new Point(6,6);
+var start = new Point(1,1);
+var startPoint = new Node(calcH(start,destinationPoint),0,null,start);
+
+
 var grid = [
     [0,0,0,0,0,0],
     [0,0,0,1,0,0],
@@ -17,7 +20,24 @@ var closedList = [];
 
 while(openList.length > 0){
     currentPoint = getSquareWithLowestFScore();
-    currentPoint.getAdjacentTiles();
+    closedList.push(currentPoint);
+    removeFromArray(openList,currentPoint);
+
+    if(arrayContainsPoint(closedList,destinationPoint)){
+        break;
+    }
+
+    var adjacentTiles = currentPoint.getAdjacentTiles();
+    for(var i = 0;i<adjacentTiles.length;i++){
+        if(arrayContaintsObject(closedList,adjacentTiles[i])){
+            continue;
+        }else if(!arrayContaintsObject(openList,adjacentTiles[i])){
+            var node = new Node();//needs to be calculated
+            openList.push(node)
+        }else if(true){
+            
+        }
+    }
 }
 
 function calcH(point,endPoint){
@@ -28,8 +48,36 @@ function calcH(point,endPoint){
 }
 
 function getSquareWithLowestFScore(){
-
+    var lowestF;
     for(var i = 0; i < openList.length;i++){
-
+        if(openList[0].f < lowestF){
+            lowestF = openList[0];
+        }
     }
+}
+
+function removeFromArray(array,object){
+    for(var i = 0;i<array.length;i++){
+        if(array[i] === object){
+            array.splice(i,1);
+        }
+    }
+}
+
+function arrayContainsPoint(array,object){
+    for(var i = 0;i < array.length;i++){
+        if(array[i].point.x == object.x && array[i].point.y == object.y){
+            return true;
+        }
+    }
+    return false;
+}
+
+function arrayContaintsObject(array,object){
+    for(var i = 0;i < array.length;i++){
+        if(array[i] === object){
+            return true;
+        }
+    }
+    return false;
 }
