5b2e3a2f8d15bc1ab33ae78acdb06d673c88bdd5
Paul Boon <gvr37.p.l.boon@gmail.com>
2015-08-11 01:14:12 +0200

    added more logic. node values are now calculated when they get added to the openlist

--- pathFinding/js/Main.js
+++ pathFinding/js/Main.js
@@ -16,34 +16,40 @@ var start = grid[1][1];
 
 var openList = [];
 var closedList = [];
-
+start.addToOpenList();
 
 while(openList.length > 0){
     var currentNode = getNodeWithLowestF();
     openList.splice(openList.indexOf(currentNode),1);
     closedList.push(currentNode);
 
-    if(currentNode.point == destination){
+    if(currentNode == destination){
         break;
     }
 
     currentNode.getNeighbours().forEach(function(neighbour){
-        
+        if(!walkableGrid[neighbour.point.x][neighbour.point.y] || closedList.indexOf(neighbour) == -1){
+            continue;
+        }
+        var newMovementCostToNeighbour = currentNode.g + GetDistance(currentNode, neighbour);
+        if(0 || openList.indexOf(neighbour) == -1){
+            //add some stuff here
+            if(openList.indexOf(neighbour) != -1){
+                openList.push(neighbour);
+            }
+        }
+
     });
 }
 
 function getNodeWithLowestF(){
     var nodeWithLowestF = null;
     for(var i = 0;i < openList.length;i++){
-        if(openList[i].f < nodeWithLowestF.f){
+        if(openList[i].f < nodeWithLowestF.f || openList[i].f == nodeWithLowestF.f && openList[i].h < nodeWithLowestF.h){
             nodeWithLowestF = openList[i];
         }
     }
     return nodeWithLowestF;
-}
-
-function addNodeToList(node){
-
 }
 
 function createGrid(){
@@ -55,6 +61,7 @@ function createGrid(){
             grid[x][y] = new Node(new Point(x,y));
         }
     }
+    return grid;
 }
 
 function create2DArray(x,y){
--- pathFinding/js/Node.js
+++ pathFinding/js/Node.js
@@ -1,8 +1,8 @@
 function Node(point){
     this.point = point;
-    this.g = this.getG();
+    this.g = null;
     this.h = null;
-    this.f = this.g+this.h;
+    this.f = null;
     this.parent = parent;
 }
 Node.prototype.draw = function(ctxt){
@@ -14,14 +14,17 @@ Node.prototype.draw = function(ctxt){
     ctxt.fillText(this.parent.point.x + "," + this.parent.point.y,this.point.x,this.point.y);
 };
 
-Node.prototype.getG = function(){
-    return destination.x - this.point.x + destination.y - this.point.y;
+Node.prototype.addToOpenList = function(h){
+    this.g = this.getG();
+    this.h = h;
+    this.f = this.g+this.h;
+    openList.push(this);
 };
 
 Node.prototype.getNeighbours = function(){
     var neighbours = [];
 };
 
-Node.prototype.addNodeToList = function(node){
-    
+Node.prototype.getG = function(){
+    return destination.x - this.point.x + destination.y - this.point.y;
 };
