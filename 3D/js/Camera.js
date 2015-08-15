c65f791470037a3b6f369bc25af00232ce1496a3
Paul Boon <gvr37.p.l.boon@gmail.com>
2015-08-15 23:17:47 +0200

    apparently you need the coordinates of the viewer and the camera dafuq?

--- 3D/js/Camera.js
+++ 3D/js/Camera.js
@@ -10,3 +10,13 @@ Camera.prototype = Object.create(Vector.prototype);
 Camera.prototype.drawVector = function(vector){
       ctxt.fillRect(vector.x,vector.y,10,10);
 };
+
+Camera.prototype.convertTo2D = function(vector){
+    var d = new Vector();
+    d.x = 0;
+    d.y = 0;
+    d.z = 0;
+
+    var bx = (ez / dz) * dx - ex;
+    var by = (ez / dz) * dy - ey;
+};
--- 3D/js/Main.js
+++ 3D/js/Main.js
@@ -2,6 +2,7 @@ var canvas = document.getElementById("mycanvas");
 canvas.addEventListener("click",doClick);
 var ctxt = canvas.getContext("2d");
 
+var viewer = new Vector(0,0,0);
 var camera = new Camera(0,0,0);
 var vector = new Vector(10,10,0);
 camera.drawVector(vector);
