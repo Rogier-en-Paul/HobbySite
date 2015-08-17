593b9d799f079d40746b1e05e81f2ea8d30335d6
Paul Boon <gvr37.p.l.boon@gmail.com>
2015-08-17 13:58:06 +0200

    copied formula from wikipedia into program

--- 3D/js/Camera.js
+++ 3D/js/Camera.js
@@ -1,8 +1,8 @@
 function Camera(x,y,z){
-    Vector.call(this,x,y,z)
-    this.yaw = 0;
-    this.pitch = 0;
-    this.roll = 0;
+    Vector.call(this,x,y,z);
+    this.ex = 0;
+    this.ey = 0;
+    this.ez = 0;
 }
 
 Camera.prototype = Object.create(Vector.prototype);
@@ -13,10 +13,23 @@ Camera.prototype.drawVector = function(vector){
 
 Camera.prototype.convertTo2D = function(vector){
     var d = new Vector();
-    d.x = 0;
-    d.y = 0;
-    d.z = 0;
+    var rx = vector.x - this.x;
+    var ry = vector.y - this.y;
+    var rz = vector.z - this.z;
 
-    var bx = (ez / dz) * dx - ex;
-    var by = (ez / dz) * dy - ey;
+    var cx = Math.cos(this.ex);
+    var cy = Math.cos(this.ey);
+    var cz = Math.cos(this.ez);
+    var sx = Math.sin(this.ex);
+    var sy = Math.sin(this.ey);
+    var sz = Math.sin(this.ez);
+
+    d.x = cy * (sz * ry + cz * rx) - sy * rz;
+    d.y = sx * (cy * rz + sy * (sz * ry + cz * rx)) + cx * (cz * ry - sz * rx);
+    d.z = cx * (cy * rz + sy * (sz * ry + cz * rx)) - sx * (cz * ry - sz * rx);
+
+    var bx = (viewer.z / d.z) * d.x - viewer.x;
+    var by = (viewer.z / d.z) * d.y - viewer.y;
+
+    console.log("x = " + bx + ",y = " + by);
 };
--- 3D/js/Main.js
+++ 3D/js/Main.js
@@ -2,10 +2,11 @@ var canvas = document.getElementById("mycanvas");
 canvas.addEventListener("click",doClick);
 var ctxt = canvas.getContext("2d");
 
-var viewer = new Vector(0,0,0);
-var camera = new Camera(0,0,0);
-var vector = new Vector(10,10,0);
+var viewer = new Vector(0,0,-3);
+var camera = new Camera(-20,0,0);
+var vector = new Vector(1,2,100);
 camera.drawVector(vector);
+camera.convertTo2D(vector);
 
 function doClick(){
 
