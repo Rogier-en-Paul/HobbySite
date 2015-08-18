f7c8261ac267e21f522f8246bc13f9ff7d57ac8d
Paul Boon <gvr37.p.l.boon@gmail.com>
2015-08-18 10:53:04 +0200

    had a talk with Lars about the meaning of some of the variables and changed some to make more sense

--- 3D/js/Main.js
+++ 3D/js/Main.js
@@ -2,8 +2,8 @@ var canvas = document.getElementById("mycanvas");
 canvas.addEventListener("click",doClick);
 var ctxt = canvas.getContext("2d");
 
-var viewer = new Vector(0,0,-3);
-var camera = new Camera(-20,0,0);
+var viewer = new Vector(0,0,10);
+var camera = new Camera(0,0,0);
 var vector = new Vector(1,2,100);
 camera.drawVector(vector);
 camera.convertTo2D(vector);
