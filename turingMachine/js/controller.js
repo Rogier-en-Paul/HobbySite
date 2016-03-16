ac85b3c56c6348dcd5a1d8c5c2f883d0a3481e5a
Paul Boon <gvr37.p.l.boon@gmail.com>
2016-03-17 00:45:07 +0100

    added a new way for adding cards still need to figure out the add new card button
    gonna need some way for piping input of 1 program into the next one

--- turingMachine/js/controller.js
+++ turingMachine/js/controller.js
@@ -9,10 +9,13 @@ app.controller('ctrl',function($scope){
     $scope.moveOptions = moveOptions;
     $scope.tape = tape;
     $scope.addCard = addCard;
+    $scope.run = run;
 
     programs[0] = new Program();//prints alternating 1s and 0s to the right
     programs[0].cards[1] = new Card(new Option(1, 1, 2), new Option(1, 1, 1));
     programs[0].cards[2] = new Card(new Option(0, 1, 1), new Option(1, 1, 1));
+    programs[0].cards[3] = new Card(new Option(0, 1, 1), new Option(1, 1, 1));
+    programs[0].cards[4] = new Card(new Option(0, 1, 1), new Option(1, 1, 1));
 
     programs[2] = new Program();
 
@@ -23,4 +26,8 @@ app.controller('ctrl',function($scope){
 
 function addCard(programN, cardN, write, move, nextCard){
     programs[parseInt(programN)].cards[parseInt(cardN)] = new Card(new Option(write, move, parseInt(nextCard)), new Option(1, 1, 1));
+}
+
+function run(){
+    tape = programs[0].execute(tape);
 }
