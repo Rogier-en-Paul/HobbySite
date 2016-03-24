9fdab7f662ddc464ae36027b413bc8c33e8d4965
Paul Boon <gvr37.p.l.boon@gmail.com>
2016-03-25 00:08:09 +0100

    looks like it's working mongoose came to the rescue working with bare mongo is pretty hard luckily i still had smitha milly's example in the future i could look more into mongoose for more advanced querys could also put logic server side now it's possible for people to see the answer maybe do a little more ui work but not that nescesaary is already a lot better than the previous version

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -1,11 +1,14 @@
-//var socket = io.connect();
+var socket = io.connect("http://localhost:8000");
 var words = ["pizza","piraat","edelsteen","discobal","hearthstone","koffieautomaat"];
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var app = angular.module('app', []);
 
+socket.emit("get");
+
 app.controller('ctrl',function($scope){
-    $scope.word = words[Math.floor(Math.random() * words.length)];
-    var nUniqueLetters = countUniqueLetters($scope.word);
+    $scope.previousWinner = "loading";
+    $scope.word;
+    var nUniqueLetters;
     $scope.maxWrongGuesses = 9;
     $scope.wrongGuesses = 0;
     $scope.rightGuesses = 0;
@@ -13,13 +16,28 @@ app.controller('ctrl',function($scope){
     $scope.guessedChars = [];
     $scope.guess = guess;
 
+    socket.on("get",function(data){
+        $scope.word = data.word;
+        $scope.previousWinner = data.name;
+        nUniqueLetters = countUniqueLetters(data.word);
+        $scope.$apply();
+    });
 
     function guess(index){
         if($scope.word.indexOf($scope.remaining[index]) > -1){
             $scope.rightGuesses++;
             if($scope.rightGuesses == nUniqueLetters){
                 reset();
-                alert("won");
+                var name = prompt("you won enter your name");
+                var newWord = prompt("enter word for next visitor");
+                if(isLegalWord(newWord) && newWord.length > 3 && newWord.length < 20){
+                    socket.emit("save", {"name":name,"word":newWord});
+                    $scope.word = newWord;
+                    $scope.previousWinner = name;
+                    reset();
+                }else{
+                    alert("illegal word");
+                }
                 return;
             }
         }else{
@@ -38,6 +56,7 @@ app.controller('ctrl',function($scope){
         $scope.wrongGuesses = 0;
         $scope.guessedChars = [];
         $scope.remaining = alphabet.slice();
+        nUniqueLetters = countUniqueLetters($scope.word)
     }
 });
 
@@ -52,4 +71,17 @@ function countUniqueLetters(string){
         }
     }
     return uniques;
+}
+
+function isLegalWord(string){
+    if(string == null){
+        return false;
+    }
+    for(var i = 0;i < string.length; i++){
+        var char = string[i];
+        if(alphabet.indexOf(char) == -1){
+            return false;
+        }
+    }
+    return true;
 }
--- hangman/server.js
+++ hangman/server.js
@@ -1,31 +1,55 @@
 var express = require("express"),
     app = express(),
     server = require("http").createServer(app),
-    io = require("socket.io").listen(server);
-
-var url = "mongodb://paul:paull@ds043180.mlab.com:43180/hangman";
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
-MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
-    console.log("Connected correctly to server.");
-    db.close();
-});
-
-
-
-
-
-
-server.listen(8000);
-app.use(express.static(__dirname + '/../'));
+    io = require("socket.io").listen(server),
+    mongoose = require("mongoose");
+app.use(express.static(__dirname + '/'));
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + "hangman.html");
+    res.sendFile(__dirname + "/hangman.html");
 });
-console.log("listening on port http://localhost:8000");
+server.listen(8000);
+console.log("listening on port 8000");
+var url = "mongodb://localhost:27017/test";
+mongoose.connect(url,function(err){
+    if(err){
+        console.log(err);
+    }else{
+        console.log("connected succesfully to mongodb");
+    }
+});
+var wordSchema = mongoose.Schema({
+    name:String,
+    word:String
+});
+var Word = mongoose.model('word',wordSchema);
 
-io.sockets.on('connection', function (socket) {
+io.sockets.on("connection",function(socket){
+    console.log("client connected");
+    socket.on("get",function(){
+        console.log("received get request");
+        Word.find(function(err, docs){
+            socket.emit("get",docs[0]);
+        }).limit(1).sort({_id:-1});
+    });
 
+    socket.on("save",function(data){
+        var newWord = new Word({name:data.name,word:data.word});
+        newWord.save(function(err,doc){
+            console.log("saved:" + doc)
+        });
+    });
 });
 
 
+
+
+
+
+var newWord = new Word({name:"wietse",word:"swag"});
+
+//newWord.save(function(err,doc){
+//    console.log(doc)
+//});
+//Word.find(function(err, docs){
+//    console.log(docs)
+//});
