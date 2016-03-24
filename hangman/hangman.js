4a54f5054c528e9896d0f678adc730358c6906ef
Paul Boon <gvr37.p.l.boon@gmail.com>
2016-03-24 16:33:13 +0100

    made it bigger better faster stronger using angular and socket.io

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -1,133 +1,55 @@
-$( document ).ready(function() {
-    var socket = io.connect();
-    var words = ["pizza","piraat","edelsteen","discobal","hearthstone","koffieautomaat"];
-    var word = words[Math.floor(Math.random() * words.length)];
-    var previousPlayer = "not initialized";
+//var socket = io.connect();
+var words = ["pizza","piraat","edelsteen","discobal","hearthstone","koffieautomaat"];
+var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var app = angular.module('app', []);
 
-    var allowedChars = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-    var guessedChars = [];
-    var nOfWrongGuesses = 0;
-    var maxWrongGuesses = 9;
-    var answer;
-    
-    reset();
+app.controller('ctrl',function($scope){
+    $scope.word = words[Math.floor(Math.random() * words.length)];
+    var nUniqueLetters = countUniqueLetters($scope.word);
+    $scope.maxWrongGuesses = 9;
+    $scope.wrongGuesses = 0;
+    $scope.rightGuesses = 0;
+    $scope.remaining = alphabet.slice();
+    $scope.guessedChars = [];
+    $scope.guess = guess;
 
-    $("#submit").on("click",function(){
-        var guess = $("#guess").val();
-        if(guess == ""){
-            return;
-        }
-        guessedChars.push(guess);
-        
-        var guessedRight = false;
-        for(var i = 0;i<word.length;i++){
-            if(guess == word[i]){
-                guessedRight = true;
-                break;
-            }
-        }
-        if(!guessedRight){
-            nOfWrongGuesses++;
-            if(nOfWrongGuesses >= maxWrongGuesses){
-                alert("u lost but the answer was: " + word);
+
+    function guess(index){
+        if($scope.word.indexOf($scope.remaining[index]) > -1){
+            $scope.rightGuesses++;
+            if($scope.rightGuesses == nUniqueLetters){
                 reset();
+                alert("won");
+                return;
             }
-        }
-        set();
-        if(answer == word){
-            alert("such win");
-            var name = prompt("enter your name");
-            var newWord = prompt("enter word for next visitor");
-            if(isAllowed(newWord)){
-                socket.emit("won",{name:name,newWord:newWord});
-            }else{
-                alert("word wasn't saved because it contained illegal characters or was null");
-            }
-
-            reset();
-        }
-        $("#guess").val("");
-    });
-    
-    $("#guess").on("keyup",function(e){
-        var input = String.fromCharCode(e.keyCode).toLowerCase();
-        for(var i = 0;i< guessedChars.length;i++){
-            if(input == guessedChars[i]){
-                $("#alerts").html("character has already been guessed");
-                $("#guess").val("");
+        }else{
+            $scope.wrongGuesses++;
+            if($scope.wrongGuesses == $scope.maxWrongGuesses){
+                reset();
                 return;
             }
         }
-        for(var i = 0;i< allowedChars.length;i++){
-            if(input == allowedChars[i]){
-                $("#guess").val(input);
-                $("#alerts").html("");
-                return;
-            }    
-        }
-        $("#alerts").html("character is not allowed");
-        $("#guess").val("");
-    });
-    
-    $("#guess").on("keydown",function(e){
-        $("#guess").val("");
-    });
-
-    socket.on("requestWord",function(data){
-        console.log(data);
-        word = data.word;
-        previousPlayer = data.name;
-        setAnswer();
-    });
+        $scope.guessedChars.push($scope.remaining[index]);
+        $scope.remaining.splice(index, 1);
+    }
 
     function reset(){
-        socket.emit("requestWord");
-        console.log(word);
-        guessedChars = [];
-        nOfWrongGuesses = 0;
-        set();
+        $scope.rightGuesses = 0;
+        $scope.wrongGuesses = 0;
+        $scope.guessedChars = [];
+        $scope.remaining = alphabet.slice();
     }
-    
-    function set(){
-        $("#guessedChars").html("guessed characters: " + guessedChars);
-        $("#counter").html("number of wrong guesses: " + nOfWrongGuesses + "/" + maxWrongGuesses);
-        setAnswer();
-    }
-    
-    function setAnswer(){
-        answer = "";
-        for(var i = 0;i<word.length;i++){
-            var found = false;
-            for(var j = 0;j<guessedChars.length;j++){
-                if(word[i] == guessedChars[j]){
-                    answer += guessedChars[j];
-                    found = true;
-                    break;
-                }
-            }
-            if(!found){
-                answer += "-";
-            }
-        }
-        $("#answer").val(answer);
-    }
-
-    function isAllowed(word){
-        if(word == null){
-            return false;
-        }
-        for(var i = 0;i< word.length;i++){
-            var foundChar = false;
-            for(var j = 0;j<allowedChars.length;j++){
-                if(word[i] == allowedChars[j]){
-                    foundChar = true;
-                }
-            }
-            if(foundChar == false){
-                return false;
-            }
-        }
-        return true;
-    }
-    
 });
+
+function countUniqueLetters(string){
+    var readLetters = [];
+    var uniques = 0;
+    for(var i = 0;i < string.length; i++){
+        var char = string[i];
+        if(readLetters.indexOf(char) == -1){
+            readLetters.push(char);
+            uniques++;
+        }
+    }
+    return uniques;
+}
